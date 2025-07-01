fc28b0dbf3209e596d1367cddb48d3028c745ce6
Gustavo Medori <mail@gustavo.sh>
2025-07-01 16:09:41 -0600

    Add bare minimum text field + wk web view to navigate web pages

--- Iris/ViewController.swift
+++ Iris/ViewController.swift
@@ -6,14 +6,87 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+	private let webView = WKWebView()
+	private let addressBar = UITextField()
+	private var addressBarBottomConstraint: NSLayoutConstraint? = nil
+
+	override func loadView() {
+		view = UIView()
+		view.addSubview(webView)
+		view.addSubview(addressBar)
 	}
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
+		webView.translatesAutoresizingMaskIntoConstraints = false
+		webView.navigationDelegate = self
+
+		addressBar.translatesAutoresizingMaskIntoConstraints = false
+		addressBar.borderStyle = .roundedRect
+		addressBar.returnKeyType = .go
+		addressBar.autocapitalizationType = .none
+		addressBar.autocorrectionType = .no
+		addressBar.delegate = self
+
+		NSLayoutConstraint.activate([
+			// Web View
+			webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+			webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+			webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			// Addresss Bar
+			addressBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+			addressBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+		])
+
+		addressBarBottomConstraint = addressBar.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+		addressBarBottomConstraint?.isActive = true
+
+		let myURL = URL(string: "https://thebrowser.company")
+		let myRequest = URLRequest(url: myURL!)
+		webView.load(myRequest)
+
+	}
+}
+
+// MARK: - User Actions
+
+extension ViewController {
+	func didSubmitAddressBar(withURL url: URL) {
+		webView.load(URLRequest(url: url))
+		addressBar.text = url.absoluteString.lowercased()
+	}
+}
+
+// MARK: - ViewController + UITextFieldDelegate
+
+extension ViewController: UITextFieldDelegate {
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		//
+		guard var text = textField.text?.lowercased() else { return false }
+		if !text.starts(with: /https?:\/\//) { text.insert(contentsOf: "https://", at: text.startIndex) }
+
+		guard let url = URL(string: text) else { return false }
+		didSubmitAddressBar(withURL: url)
+		addressBar.resignFirstResponder()
+		return true
+	}
+
+	func textFieldDidBeginEditing(_ textField: UITextField) {
+		textField.selectAll(nil)
+	}
+}
+
+extension ViewController: WKNavigationDelegate {
+	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+		if let url = webView.url {
+			addressBar.text = url.absoluteString.lowercased()
+		}
+	}
 }
 
